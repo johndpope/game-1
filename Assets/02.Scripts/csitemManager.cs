b11f6aee187d23a204495aad3c22f2722b34fd3e
YangHyungMo <yhmj7@naver.com>
2016-03-28 14:04:12 +0900

    1

---
+++ Assets/02.Scripts/csitemManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class csitemManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
