1d94af7f86f25bb5ca76e727e77f57d3c0be4f68
ppooppii <ppooppii119@naver.com>
2016-04-06 11:54:39 +0900

    map & battle

---
+++ Assets/02.Scripts/csBattle.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class csBattle : MonoBehaviour {
+
+    public Scrollbar pcc;
+    public Scrollbar ecc;
+    public float pTimer = 10;
+    public float eTimer = 10;
+    float pTimer2;
+    float eTimer2;
+    public bool timerIsActive = true;
+
+    public GameObject atkbtn;
+    public GameObject runbtn;
+    public GameObject itembtn;
+    public GameObject player;
+
+
+    // Use this for initialization
+    void Start () {
+        pTimer2 = pTimer;
+        eTimer2 = eTimer;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        //cc.rectTransform.position.x();
+        timer();
+
+    }
+
+    public void timer()
+    {
+        if (timerIsActive)
+        {
+            pTimer -= Time.deltaTime;
+            eTimer -= Time.deltaTime;
+
+            pcc.value += Time.deltaTime / pTimer2;
+            ecc.value += Time.deltaTime / eTimer2;
+
+            if (pTimer <= 0)
+            {
+                pTimer = 0;
+                atkbtn.SetActive(true);
+                runbtn.SetActive(true);
+                itembtn.SetActive(true);
+                //player.SetActive(false);
+                timerIsActive = false;
+            } 
+            if(eTimer <= 0)
+            {
+                ecc.value = 0;
+                eTimer = eTimer2;
+            }           
+        }
+    }
+
+    public void atk()
+    {
+        pcc.value = 0;
+        atkbtn.SetActive(false);
+        runbtn.SetActive(false);
+        itembtn.SetActive(false);
+        //player.SetActive(true);
+        timerIsActive = true;
+        pTimer = pTimer2;
+    }
+
+}
