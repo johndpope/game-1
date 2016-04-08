35c0f7059063529efce9dabee1780ffe5f8a8d3c
ShimDonggi <simfas525@naver.com>
2016-04-08 12:27:02 +0900

    dg

---
+++ Assets/02.Scripts/EnemyControler.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+using live2d;
+
+public class EnemyControler : MonoBehaviour {
+
+
+    public int enemyUP =3;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        
+        if (Input.GetButtonDown("Fire1"))
+        {
+            //gameObject.GetComponent<main1>().mtnFiles[0].bytes =
+            gameObject.GetComponent<live2d_setting>().motion = Live2DMotion.loadMotion(gameObject.GetComponent<live2d_setting>().mtnFiles[0].bytes);
+            //gameObject.GetComponent<live2d_setting>().motion.setLoop(true);
+            gameObject.GetComponent<live2d_setting>().motionManager.startMotion(gameObject.GetComponent<live2d_setting>().motion, false);
+            enemyUP -= 1;
+            if (enemyUP == 0) {
+                onIdle();
+            }
+            
+            Debug.Log("Fire1");
+        }
+
+        if (Input.GetButtonDown("Fire2"))
+        {
+            //motionManager.stopAllMotions();
+            Debug.Log("Jump");
+        }
+        
+    }
+
+
+
+    public void onIdle()
+    {
+        //iTween.MoveBy(gameObject, iTween.Hash("x", 1, "easeType", "", "loopType", "", "delay", .1));
+        gameObject.GetComponent<live2d_setting>().motion = Live2DMotion.loadMotion(gameObject.GetComponent<live2d_setting>().mtnFiles[1].bytes);
+        gameObject.GetComponent<live2d_setting>().motionManager.startMotion(gameObject.GetComponent<live2d_setting>().motion, false);
+        enemyUP = 3;
+
+    }
+    
+
+}
---
+++ Assets/02.Scripts/live2d_setting.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using live2d;
+using live2d.framework;
+using System.Text.RegularExpressions;
+
+[ExecuteInEditMode]
+public class live2d_setting : MonoBehaviour
+{
+    public TextAsset mocFile;
+    public Texture2D[] textures;
+    public TextAsset[] mtnFiles;
+
+    public Live2DModelUnity live2DModel;
+
+    public Live2DMotion motion;
+    public MotionQueueManager motionManager;
+    public TextAsset modelJson;
+
+ 
+    // Use this for initialization
+    void Start()
+    {
+
+        Live2D.init();
+        //motion = Live2DMotion.loadMotion(mtnFiles[0].bytes);
+        //motion.setLoop(true);
+ 
+        live2DModel = Live2DModelUnity.loadModel(mocFile.bytes);
+        for (int i = 0; i < textures.Length; i++)
+        {
+            live2DModel.setTexture(i, textures[i]);
+        }
+        motionManager = new MotionQueueManager();
+      
+        motionManager.startMotion(motion, false);
+
+    }
+ 
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            motion = Live2DMotion.loadMotion(mtnFiles[0].bytes);
+            motion.setLoop(true);
+            motionManager.startMotion(motion, false);
+            Debug.Log("Fire1");
+        }
+        if (Input.GetButtonDown("Fire2"))
+        {
+            motionManager.stopAllMotions();
+            Debug.Log("Jump");
+        }
+
+        float modelWidth = live2DModel.getCanvasWidth();
+        Matrix4x4 m1 = Matrix4x4.Ortho(
+                0, modelWidth, modelWidth,
+                0, -50.0f, 50.0f);
+        Matrix4x4 m2 = transform.localToWorldMatrix;
+        Matrix4x4 m3 = m2 * m1;
+
+        motionManager.updateParam(live2DModel);
+        if (live2DModel == null) return;
+
+        live2DModel.setMatrix(m3);
+        live2DModel.update();
+    }
+
+    void OnRenderObject()
+    {
+        live2DModel.draw();
+    }
+}
